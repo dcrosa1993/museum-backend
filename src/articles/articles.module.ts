4ecbda23357f89b50d9b5d960e47265d668f847f
Denismay Concepcion Rosa <95624156+dcrosa1993@users.noreply.github.com>
2026-09-20 15:09:57 -0400

    feat: add AuthModule to ArticlesModule imports

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesController } from './articles.controller.js';
 import { ArticlesService } from './articles.service.js';
 import { Article } from './entities/article.entity.js';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article])],
+  imports: [TypeOrmModule.forFeature([Article]), AuthModule],
   controllers: [ArticlesController],
-  providers: [ArticlesService]
+  providers: [ArticlesService],
 })
 export class ArticlesModule {}
