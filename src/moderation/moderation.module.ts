91a8f23a431d7f5150c178e8aa49bef2a8ad26fb
Denismay Concepcion Rosa <95624156+dcrosa1993@users.noreply.github.com>
2026-09-20 15:12:58 -0400

    feat: add AuthModule to ModerationModule imports

--- src/moderation/moderation.module.ts
+++ src/moderation/moderation.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModerationController } from './moderation.controller.js';
 import { ModerationService } from './moderation.service.js';
 import { Article } from '../articles/entities/article.entity.js';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Article])],
+  imports: [TypeOrmModule.forFeature([Article]), AuthModule],
   controllers: [ModerationController],
   providers: [ModerationService],
 })
